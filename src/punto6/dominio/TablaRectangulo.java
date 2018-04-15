ad8aebc99b47005e54fea5bc75ec05e50a651787
LENOVO <LENOVO@LAPTOP-HPG5DB6R>
2018-04-15 00:08:22 -0300

    MODIFICADO EL PUNTO6 gestor

--- src/punto6/dominio/TablaRectangulo.java
+++ src/punto6/dominio/TablaRectangulo.java
@@ -3,69 +3,58 @@ package punto6.dominio;
 
 
 public class TablaRectangulo {
-     private String base;
-   private String altura;
-   private String punto;
-   private String superficie;
-   private String perimetro;
-   public TablaRectangulo(String base,String altura,String punto,String superficie,String perimtro){
-       this.base=base;
-       this.altura=altura;
-       this.punto=punto;
-       this.superficie=superficie;
-       this.perimetro=perimtro;
-   }
-    public void mostar(){
-       System.out.println("Base:"+getBase());
-       System.out.println("Altura"+getAltura());
-       System.out.println("Punto:"+getPunto());
-       System.out.println("Superficie:"+getSuperficie());
-       System.out.println("Perimetro:"+getPerimetro());
-   }
+   private double base;
+   private double altura;
+   private double punto;
+   private double superficie;
+   private double perimetro;
 
-    public String getBase() {
-        return base;
-    }
-
-    public void setBase(String base) {
+    public TablaRectangulo(double base, double altura, double punto, double superficie, double perimetro) {
         this.base = base;
-    }
-
-    public String getAltura() {
-        return altura;
-    }
-
-    public void setAltura(String altura) {
         this.altura = altura;
-    }
-
-    public String getPunto() {
-        return punto;
-    }
-
-    public void setPunto(String punto) {
         this.punto = punto;
-    }
-
-    public String getSuperficie() {
-        return superficie;
-    }
-
-    public void setSuperficie(String superficie) {
         this.superficie = superficie;
-    }
-
-    public String getPerimetro() {
-        return perimetro;
-    }
-
-    public void setPerimetro(String perimetro) {
         this.perimetro = perimetro;
     }
 
-    @Override
-    public String toString() {
-        return "{" + "base=" + base + ", altura=" + altura + ", punto=" + punto + ", superficie=" + superficie + ", perimetro=" + perimetro + '}';
+    public double getBase() {
+        return base;
     }
 
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPunto() {
+        return punto;
+    }
+
+    public void setPunto(double punto) {
+        this.punto = punto;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+   
 }
