2303a4526b70457eb8159b5aa16fd48016ebc20d
LENOVO <LENOVO@LAPTOP-HPG5DB6R>
2018-04-15 00:16:19 -0300

    COMPLETADO PUNTO 1-11

--- src/punto12/dominio/GestorLibro.java
+++ src/punto12/dominio/GestorLibro.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 
 package punto12.dominio;
 
-/**
- * 
- * @author Sergio Romero
- */
+
 public class GestorLibro {
 
 }
--- src/punto12/test/ProgramaPrincipal.java
+++ src/punto12/test/ProgramaPrincipal.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package punto12.test;
 
-/**
- * 
- * @author Sergio Romero
- */
+
 public class ProgramaPrincipal {
     public static void main(String[] args){
         
